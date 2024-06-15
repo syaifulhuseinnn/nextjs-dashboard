e2147b06643171360e4069df471a4c41687430a6
syaifulhuseinnn <syaifulhuseinnn@gmail.com>
2024-06-15 15:22:46 +0700

    Chapter 11: Adding Search and Pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,7 +10,7 @@ import {
 } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
 
-export default async function Page() {
+export default async function Page(props: any) {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
